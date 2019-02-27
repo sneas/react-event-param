88bbd799e3def7f14317126266c6d00de8ac7efe
Dima Snisarenko <snisarenkodima@gmail.com>
2019-02-27 20:41:27 +0100

    feat(lib): Add setEventParam and getEventParam methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-// Libraries's code goes here
+const PROP_NAME = 'event-param';
+
+export const setEventParam = (value: any) => {
+    return {
+        [PROP_NAME]: JSON.stringify(value),
+    };
+};
+
+export const getEventParam = <T>(startNode: HTMLElement): T => {
+    let node: HTMLElement | null = startNode;
+
+    do {
+        if (node.getAttribute(PROP_NAME) !== null) {
+            return <T>JSON.parse(<string>node.getAttribute(PROP_NAME));
+        }
+
+        node = node.parentElement;
+    } while (node !== null);
+
+    throw new Error(`Couldn't find node with attribute ${PROP_NAME}`);
+};
